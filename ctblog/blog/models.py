bee9df4504238f32e630275c08846480128958aa
armenuhitoroyan <armenuhi.h.toroyan@gmail.com>
2024-02-04 23:41:33 +0400

    added contact table

--- ctblog/blog/models.py
+++ ctblog/blog/models.py
@@ -8,3 +8,13 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
