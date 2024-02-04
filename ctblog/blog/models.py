d018eb74c0cbdaf24979311b0c59e40581cf544a
armenuhitoroyan <armenuhi.h.toroyan@gmail.com>
2024-02-05 02:34:29 +0400

    added images

--- ctblog/blog/models.py
+++ ctblog/blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    image = models.ImageField(upload_to="blog_posts/", default=None, null=True, blank=True)
 
     def __str__(self):
         return self.title
--- ctblog/blog/urls.py
+++ ctblog/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 # from .views import home_page, contact_us, blog_detail
 from .views import home_page, contact_us, blog_detail
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 app_name = "blog"
@@ -9,4 +11,4 @@ urlpatterns = [
     path('', home_page, name='home'),
     path('contact-us/', contact_us, name='contact-us'),
     path('blog-detail/<int:pk>/', blog_detail, name='blog-detail'),  # pk primary key == id
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
