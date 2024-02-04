fe1b5c52ea64a242075ee3f8786b56ec73aad6b3
armenuhitoroyan <armenuhi.h.toroyan@gmail.com>
2024-02-04 23:00:40 +0400

    data is separated by id

--- ctblog/blog/urls.py
+++ ctblog/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_page, contact_us
+from .views import home_page, contact_us, blog_detail
 
 urlpatterns = [
     path('', home_page),
-    path('contact-us/', contact_us)
+    path('contact-us/', contact_us),
+    path('blog-detail/<int:pk>/', blog_detail),  # pk primary key == id
 ]
--- ctblog/blog/views.py
+++ ctblog/blog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, HttpResponse
+from .models import Blog
 
 # Create your views here.
 
 def home_page(request):
+
+    context = {}
+    blogs = Blog.objects.all()
+    context["blogs"] = blogs
     # return HttpResponse("Welcome to CT Blog!")
-    return render(request, "home.html", {})
+    return render(request, "home.html", context)
 
 def contact_us(request):
     # return HttpResponse("Welcome to Contact Us page!")
     return render(request, "contact_us.html", {})
+
+def blog_detail(request, pk):  # pk for details' id
+    context = {}
+    blog = Blog.objects.get(id=pk)
+    context["blog"] = blog
+
+    return render(request, "blog_detail.html", context)
